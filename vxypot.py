42c67a4546e08ff12fe03cfdc9dd195c5d5c8263
Panadestein <rpana92@gmail.com>
2019-09-12 14:14:00 +0200

    Using Tensorly for n-mode tensor product

--- vxypot.py
+++ vxypot.py
@@ -1,12 +1,13 @@
 """
 The present module performs the optimization of the coefficients of the SOP-FBR
 representation of a 2D PES of the shape (V(x, y) = x^2 + y^2 + lambda*xy)
+It makes use of Numpy and Tensorly packages
 """
 import os
 import time
-import itertools
 import numpy as np
 from numpy.polynomial import chebyshev as cheby
+import tensorly as tl
 import nlopt
 
 # System paramters
@@ -27,54 +28,27 @@ def v2d(x_dim, y_dim):
 
 
 def vchpot(q_array, c_cheb, c_comb):
-    """ Computes the value of the SOP-FBR potential. The workflow
-        of this function can be understood as follows:
-
-        The variables c_cheb and c_comb contain the values of the
-        Chebyshev (Cljk) and interaction cofficients (Cj1...jk) respectively.
-
-        Then we form all the possible combinations of the indexes than conform
-        Cj1...jk using the values of m_k (MARRAY). This is achieved with the
-        itertools function enumerate(itertools.product(*mcomb)). Iterating by
-        this array we are effectively performing the nested sums.
-
-        Second we define the hartree_product variable, that will contain the
-        products of the SPPs for a given k (loop over NDOF). This products
-        are made of sums of Chebyshev polynomials, wich are formed using
-        the pol_tk, pol_mk and pol variables.
-
-        The variable pol_tk contains the array of the Chebyshev coefficients
-        splitted acording to the dimension of the polinomials (CHEBDIM), after
-        performing this operation we have the Cljk separated by tk (CHEBDIM).
-        Next the resulting array needs to be separated by k, which is achieved
-        in the array pol_mk. This part may result a little bit tricky, so it
-        will be further explained: for the general case (when we have different
-        m_k) we have to split the array pol_tk (which is an array of subarrays
-        all with size t_k) into an array of subarrays of subarrays (k, jk, tk),
-        so the numpy function cumsum (cumulative sum) may come handy here. Take
-        the hypotetical case of tk=2, k=3 and the m_k = [2, 3, 5], here the
-        split of pol_tk must be done in 3 chunks of dimension :2, 2:5, 5: and
-        every single chunk will contain (2, 3, 5) arrays of dimension 2.
-
-        The set of indexes runing in one iteration of the sum (that is, the
-        current jk's) is given by the comb variable, we will use this
-        information to form the current  iteration's Chebyshev polynomials
-        coefficients (stored in the variable pol). So in pol_mk, we first
-        select the value of i (k) and then the current jk (comb[i]).
-        The actual polynomials are created with cheby.chebval, and
-        further avaluated in the requested grid point q_array[i].
+    """ Computes the value of the SOP-FBR potential by first
+        conforming the vij(k) matrixes, then reshaping the core
+        tensor, and penforming the tensor dot product.
     """
-    vpot = 0
-    mcomb = [np.arange(i) for i in MARRAY]
-    pol_tk = np.array(np.split(c_cheb, c_cheb.shape[0] / CHEBDIM))
-    pol_mk = np.array(np.split(pol_tk, np.cumsum(MARRAY))[0:-1])
-    for idx, comb in enumerate(itertools.product(*mcomb)):
-        hartree_product = 1
-        for i in np.arange(NDOF):
-            pol = pol_mk[i][comb[i]]
-            hartree_product *= cheby.chebval(q_array[i], pol)
-        vpot += c_comb[idx] * hartree_product
-    return vpot
+    coeff_tk = np.array(np.split(c_cheb, c_cheb.shape[0] / CHEBDIM))
+    chev_coeff = np.array(np.split(coeff_tk, np.cumsum(MARRAY))[0:-1])
+    v_matrixes = []
+    for kdof, m_kp in enumerate(MARRAY):
+        v_kp = np.zeros((q_array.shape[0], m_kp))
+        for i_kp, val in enumerate(q_array):
+            for j_kp in np.arange(m_kp):
+                v_kp[i_kp, j_kp] = cheby.chebval(
+                    val[kdof], chev_coeff[kdof][j_kp])
+        v_matrixes.append(v_kp)
+    v_matrixes = np.array(v_matrixes)
+
+    prod = c_comb.reshape(MARRAY)
+    for idx, elem in enumerate(v_matrixes):
+        prod = tl.tenalg.mode_dot(prod, elem, idx)
+
+    return prod
 
 
 # RMS
@@ -90,10 +64,7 @@ def rho(carray, grad):
 
     c_cheb = carray[:NCHEB]
     c_comb = carray[NCHEB::]
-    e_vch = []
-    for elem in G_AB:
-        e_vch.append(vchpot(elem, c_cheb, c_comb))
-    e_vch = np.array(e_vch)
+    e_vch = vchpot(G_AB, c_cheb, c_comb)
     rms = np.sqrt(((e_vch - E_AB) ** 2).mean())
 
     with open("rms", "a") as file_target:
@@ -112,10 +83,10 @@ def rho(carray, grad):
 # Reference data and parameter guess input
 
 
-X = np.linspace(-50, 50, num=100)
-Y = np.linspace(-50, 50, num=100)
+X = np.linspace(-50, 50, num=15)
+Y = np.linspace(-50, 50, num=15)
 G_AB = np.concatenate((X[:, None], Y[:, None]), axis=1)
-E_AB = np.vectorize(v2d)(X, Y)
+E_AB = v2d(X[:, None], Y[None, :])
 np.savetxt("e_ref", E_AB)
 
 # Total number of Chebyshev polinomial's coefficients
@@ -129,12 +100,14 @@ C_CHEB = np.random.uniform(-60, 60, size=NCHEB)
 
 # Initial Configuration interaction
 C_COMB = np.random.uniform(low=0., high=1., size=NCOMB)
+
+# Total parameter array and dimension
 CARRAY = np.concatenate((C_CHEB, C_COMB))
+PARDIM = CARRAY.shape[0]
 
 # Fitting process
 
 PDEV = 0.3
-PARDIM = CARRAY.shape[0]
 VALUE_UPPER = np.zeros(len(CARRAY))
 VALUE_LOWER = np.zeros(len(CARRAY))
 
@@ -146,7 +119,7 @@ for j, el in enumerate(CARRAY):
         VALUE_UPPER[j] = CARRAY[j] * (1.0 - PDEV)
         VALUE_LOWER[j] = CARRAY[j] * (1.0 + PDEV)
 
-MAXEVAL = 10
+MAXEVAL = 1
 MINRMS = 0.01
 
 OPT = nlopt.opt(nlopt.LN_BOBYQA, PARDIM)
@@ -164,7 +137,7 @@ with open("minrms", "w") as minrms:
 
 # Reducing directory entropy
 
-TIMESTR = time.strftime("%Y%m%d_%H%M%S")  # A unique time stamp
+TIMESTR = time.strftime("%Y%m%d_%H%M%S")  # A unique time stamp for out_dir
 OUT_DIR = "out_" + TIMESTR
 os.makedirs(OUT_DIR)
 os.rename("e_sopfbr", OUT_DIR + "/e_sopfbr")
